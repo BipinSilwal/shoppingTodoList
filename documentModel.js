ac19bd54adccbd5b5e2d8917e42f35c84a8e45f7
bipin <bpnsilwal12@gmail.com>
2023-03-07 14:13:04 +0545

    inputCart

--- documentModel.js
+++ documentModel.js
@@ -1,10 +1,12 @@
 const itemForm = document.querySelector('.item-form');
-// const searchForm = document.querySelector('.search-form');
+const searchForm = document.querySelector('.search-form');
 
 const itemInput = document.querySelector('.item-input');
 const itemList = document.querySelector('.item-list');
 
-const clearAll = document.querySelector('.clearAll');
+const clearAll = document.querySelector('.btn1');
+
+// adding item to list..................
 
 function addItemToList(e) {
   e.preventDefault();
@@ -15,17 +17,57 @@ function addItemToList(e) {
     return;
   }
 
-  addItemToDOM(newItem);
+  addItemToDOM('filterBox', newItem);
+
+  itemInput.value = '';
 }
 
-function addItemToDOM(item) {
-  let list = document.createElement('li');
-  list.className = 'filterBox';
-  list.textContent = item;
+// adding items to DOM..............
+
+function addItemToDOM(classes, item) {
+  const list = document.createElement('li');
+  list.className = classes;
+  list.appendChild(document.createTextNode(item));
+  const button = addButton('btn2');
+  list.appendChild(button);
+  // Add to UL
+  itemList.append(list);
+  console.log(itemList);
 }
 
-function addButton() {
-  let button = document.createElement('button');
+// clearing All Items...............
+
+function clearAllItem() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
+
+function removeItem(e) {
+  itemList.removeChild(e.target.parentElement);
+}
+//creating button..................
+
+function addButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  button.appendChild(document.createTextNode('X'));
+  return button;
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  console.log(items);
+
+  if (items.length === 0) {
+    clearAll.style.display = 'none';
+    searchForm.style.display = 'none';
+  } else {
+    clearAll.style.display = 'block';
+    searchForm.style.display = 'block';
+  }
 }
 
 itemForm.addEventListener('submit', addItemToList);
+clearAll.addEventListener('click', clearAllItem);
+itemList.addEventListener('click', removeItem);
